16a3b1f9a2626da1b2c0903762a7c50534d503cf
annakyung <annakoung6119@gmail.com>
2023-08-30 10:40:07 +0900

    feat : 되돌리기

--- js/visual.js
+++ js/visual.js
@@ -1,24 +1,16 @@
 // 비주얼
 
 window.addEventListener("load", function () {
-
-    const letterTop = document.querySelector(".letter-top");
-    const letterPaper = document.querySelector(".letter-paper");
-    const letterClick = document.querySelector(".letter-click");
-    const letterLogo = document.querySelector(".letter-logo");
-    letterTop.addEventListener("click", function () {
+  const letterTop = document.querySelector(".letter-top");
+  const letterPaper = document.querySelector(".letter-paper");
+  const letterClick = document.querySelector(".letter-click");
+  const letterLogo = document.querySelector(".letter-logo");
+  letterTop.addEventListener("click", function () {
     letterTop.classList.toggle("letter-top-active");
     letterPaper.classList.toggle("letter-paper-active");
     letterClick.classList.toggle("letter-click-active");
     letterLogo.classList.toggle("letter-logo-active");
-    });
+  });
 
-// 스크롤 모션에 따른 css 추가 (https://greensock.com/scrolltrigger/)
-
-
-
-
-
-
+  // 스크롤 모션에 따른 css 추가 (https://greensock.com/scrolltrigger/)
 });
-
