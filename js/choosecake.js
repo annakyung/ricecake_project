959361a5223250b49853433f4f83dfe7c9b310be
annakyung <annakoung6119@gmail.com>
2023-09-04 18:16:21 +0900

    feat : choosecake 수정

--- js/choosecake.js
+++ js/choosecake.js
@@ -1,5 +1,9 @@
 // 나에게 맞는 떡케이크 찾기
 window.addEventListener("load", function () {
+  // 추천상품
+  let CHOOSECAKE_GAME;
+  let choosecakeTag = $("#game-result-html");
+
   // data.json을 로딩 (new Date()처럼 복사해온 것)
   const xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function (event) {
@@ -13,9 +17,6 @@ window.addEventListener("load", function () {
       let obj = JSON.parse(str);
 
       CHOOSECAKE_GAME = obj.choosecakegame;
-
-      // 떡케이크 밸런스 게임을 화면에 배치
-      showChoosecakeGame();
     }
   };
 
@@ -25,69 +26,36 @@ window.addEventListener("load", function () {
   // 웹 브라우저 기능을 실행할 수 있도록 요청
   xhttp.send();
 
-  // 추천상품
-  let CHOOSECAKE_GAME;
-  let choosecakeTag = $("#data-choosecake");
-
   // =========================================
   // 추천 상품 화면 출력 기능
-  // function showRecommendGood() {
   function showChoosecakeGame(item) {
-    let html = ``;
-
-    CHOOSECAKE_GAME.forEach(function (item) {
-      let tag = `
-      <div>RESULT</div>
-      <span>당신에게 맞는 떡케이크는 이거인 것 같아요!</span>
-      <img src="${item.pic}" alt="${item.alt}">
-      <p>${item.name}</p>
-      <div class="choose-btn">
-        <button class="bt-gotoproduct">제품 보러가기</button>
-        <button class="bt-game-restart">게임 다시하기</button>
-      </div>
-      `;
-      html += tag;
-    });
-
-    choosecakeTag.innerHTML = html;
-    // const swRecommend = new Swiper(".sw-recommend", {
-    //   slidesPerView: 4, // 화면에 보여지는 갯수
-    //   spaceBetween: 40, // 슬라이드와 슬라이드의 사이 간격
-    //   slidesPerGroup: 4, // 슬라이드로 넘어가는 갯수
-    //   navigation: {
-    //     prevEl: ".recommend .slide-prev",
-    //     nextEl: ".recommend .slide-next",
-    //   },
-    //   pagination: {
-    //     el: ".recommend .slide-pg",
-    //     type: "fraction",
-    //   },
-    // });
+    let html = `
+        <img src="${item.pic && item.pic}" alt="${item.alt}">
+        <p>${item.name}</p>
+    `;
+    choosecakeTag.html(html);
   }
   // =========================================
 
   // 시작화면
   const gameStartDiv = $(".game-stage-start");
   // 시작 버튼
-  const gameStartBt = gameStartDiv.find(".bt-game-start");
+  const gameStartBt = $(".bt-game-start");
   // 게임화면
   const gameViewDiv = $(".game-stage-view");
   // 게임 각 레벨 div : 결과는 []
-  const gameLevelDivs = gameViewDiv.find(".game-level");
+  const gameLevelDivs = $(".game-level");
   // 결과화면
   const gameResultDiv = $(".game-stage-result");
-  const gameResultNum = gameResultDiv.find(".game-state-num");
   // 다시 시작하기 버튼
-  const gameRestartBt = gameResultDiv.find(".bt-game-restart");
+  const gameRestartBt = $(".bt-game-restart");
   // 결과창 닫기 버튼
-  const gamecloseBt = gameResultDiv.find(".game-result-close");
+  const gamecloseBt = $(".game-result-close");
 
   // 게임 각 레벨 div 의 총 개수 : 배열이라서 length
   let gameLevelTotal = gameLevelDivs.length;
   let gameResult = "";
   let gameLevel = 0;
-  // 현재 게임의 상태
-  let gameState = "준비";
   //게임 레벨 보여주기
   function gameShowFN() {
     gameLevelDivs.hide();
@@ -96,7 +64,6 @@ window.addEventListener("load", function () {
     const gameBtns = gameLevelDivs.eq(gameLevel).find("button");
     $.each(gameBtns, function (index, item) {
       $(item).click(function () {
-        // console.log(index);
         gameResult += index;
         gameLevel++;
         // 클릭 제거하기
@@ -105,9 +72,8 @@ window.addEventListener("load", function () {
           // 게임화면 마감
           gameViewDiv.hide();
           // 결과창으로 보여주기
-          gameResultDiv.show();
+          gameResultDiv.addClass("game-stage-result-active");
           // 결과 내용 변경해서 출력하기
-          console.log(gameResult);
           gameResultShowFN();
         } else {
           // 다음 게임
@@ -120,58 +86,75 @@ window.addEventListener("load", function () {
     switch (gameResult) {
       case "0000":
         // 단-한-무-화 = 미니 케이크 (앙금)
+        showChoosecakeGame(CHOOSECAKE_GAME[3]);
         break;
       case "0001":
         // 단-한-무-심 = 모듬 떡케이크0
+        showChoosecakeGame(CHOOSECAKE_GAME[0]);
         break;
       case "0010":
         // 단-한-색-화 = 미니 케이크 (앙금)
+        showChoosecakeGame(CHOOSECAKE_GAME[3]);
         break;
       case "0011":
         // 단-한-색-심 = 쌀 (빵) 케이크
+        showChoosecakeGame(CHOOSECAKE_GAME[4]);
         break;
       case "0100":
         // 단-여-무-화 = 미니 케이크 쌀 (빵)
+        showChoosecakeGame(CHOOSECAKE_GAME[5]);
         break;
       case "0101":
         // 단-여-무-심 = 모듬 떡 케이크
+        showChoosecakeGame(CHOOSECAKE_GAME[0]);
         break;
       case "0110":
         // 단-여-색-화 = 미니 케이크 쌀 (빵)
+        showChoosecakeGame(CHOOSECAKE_GAME[5]);
         break;
       case "0111":
         // 단-여-색-심 = 쌀 (빵) 케이크
+        showChoosecakeGame(CHOOSECAKE_GAME[4]);
         break;
 
       case "1000":
         // 개-한-무-화 = 기성품 (앙금)
+        showChoosecakeGame(CHOOSECAKE_GAME[2]);
         break;
       case "1001":
         // 개-한-무-심 = 쌀 (빵) 케이크
+        showChoosecakeGame(CHOOSECAKE_GAME[4]);
         break;
       case "1010":
         // 개-한-색-화 = 커스텀 케이크 (앙금)
+        showChoosecakeGame(CHOOSECAKE_GAME[1]);
         break;
       case "1011":
         // 개-한-색-심 = 쌀 (빵) 케이크
+        showChoosecakeGame(CHOOSECAKE_GAME[4]);
         break;
       case "1100":
         // 개-여-무-화 = 기성품 (앙금)
+        showChoosecakeGame(CHOOSECAKE_GAME[2]);
         break;
       case "1101":
         // 개-여-무-심 = 모듬 떡케이크
+        showChoosecakeGame(CHOOSECAKE_GAME[0]);
         break;
       case "1110":
         // 개-여-색-화 = 커스텀 케이크 (앙금)
+        showChoosecakeGame(CHOOSECAKE_GAME[1]);
         break;
       case "1111":
         // 개-여-색-심 = 쌀 (빵) 케이크
+        showChoosecakeGame(CHOOSECAKE_GAME[4]);
         break;
     }
   }
   // 게임 시작 클릭
   gameStartBt.click(function () {
     gameState = "게임시작";
+    gameLevel = 0;
     // 시작화면 사라지기
     gameStartDiv.hide();
     // 게임 화면 보여주기
@@ -186,7 +169,7 @@ window.addEventListener("load", function () {
     gameResult = "";
     // 결과창 숨기고
     gameLevelDivs.hide();
-    gameResultDiv.hide();
+    gameResultDiv.removeClass("game-stage-result-active");
     // 게임 진행 중인 화면
     gameViewDiv.show();
     // 게임시작
@@ -194,7 +177,7 @@ window.addEventListener("load", function () {
   });
   // 결과창 닫기
   gamecloseBt.click(function () {
-    gameResultDiv.hide();
+    gameResultDiv.removeClass("game-stage-result-active");
     gameStartDiv.show();
   });
 });
