b201d3d7e9a1dac0e9b375e211abeb1690578013
jueun song <juju1004juju@gmail.com>
2023-09-04 16:10:35 +0900

    feat: 모바일 480px 반응형

--- js/header.js
+++ js/header.js
@@ -1,2 +1,11 @@
 // 상단
-window.addEventListener("load", function () {});
+window.addEventListener("load", function () {
+
+
+    document.querySelector('.topmenu').addEventListener('click',e=>{
+        if(e.target.nodeName === 'LI'){
+          let id_value = e.target.dataset.link;
+          document.querySelector(id_value).scrollIntoView({behavior : 'smooth'});
+        }
+      });
+});
